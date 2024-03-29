0c210d85907d01d9bb994f5a6faef7da02ae6523
ethan netz <ethannetz@hotmail.com>
2024-03-29 11:34:32 -0400

    Initial Commit

---
+++ happily/happilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  happilyApp.swift
+//  happily
+//
+//  Created by Ethan Netz on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct happilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
